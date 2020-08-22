3387f79dc0ed588de6ff5d51758a6fc66cef4356
HammerMan <hammerimpact88@gmail.com>
2020-08-22 14:06:30 +0900

    [UI] Button Test

--- ImageAnalyzerApp/ImageAnalyzerApp/Form1.Designer.cs
+++ ImageAnalyzerApp/ImageAnalyzerApp/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(904, 390);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(148, 37);
+            this.button_Start.TabIndex = 2;
+            this.button_Start.Text = "Start";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Location = new System.Drawing.Point(904, 433);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(148, 37);
+            this.button_Clear.TabIndex = 3;
+            this.button_Clear.Text = "Clear";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(862, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 21);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Files";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Types";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(443, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Results";
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(29, 12);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.ReadOnly = true;
+            this.textBoxFilePath.Size = new System.Drawing.Size(827, 21);
+            this.textBoxFilePath.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1064, 489);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.button_Start);
+            this.Name = "Form1";
+            this.Text = "Image Analyzer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFilePath;
     }
 }
 
--- ImageAnalyzerApp/ImageAnalyzerApp/Form1.cs
+++ ImageAnalyzerApp/ImageAnalyzerApp/Form1.cs
@@ -12,9 +12,44 @@ namespace ImageAnalyzerApp
 {
     public partial class Form1 : Form
     {
+        // Private variables
+        int nTest = 1;
+
         public Form1()
         {
             InitializeComponent();
+
+            nTest = 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxFilePath.Text = "None";
+        }
+
+        //private void button_hello_Click(object sender, EventArgs e)
+        //{
+        //    //var result = MessageBox.Show(string.Format("isTest : {0}", nTest), "뻐큐", MessageBoxButtons.YesNo, MessageBoxIcon.Information, MessageBoxDefaultButton.Button2);
+        //    //switch (result)
+        //    //{
+        //    //    case DialogResult.Yes:
+        //    //        ++nTest;
+        //    //        break;
+
+        //    //    case DialogResult.No:
+        //    //        --nTest;
+        //    //        break;
+        //    //}
+        //}
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            textBoxFilePath.Text = "button_Start_Click";
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            textBoxFilePath.Text = "button_Clear_Click";
         }
     }
 }
